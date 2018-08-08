11d568572210ee81071968e2d6ecf85fe8f6da57
Andy-Shao <hj13035952619@126.com>
2018-08-08 13:07:33 +0800

    fix bugs

--- neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDatoClips.java
+++ neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDatoClips.java
@@ -1,8 +1,15 @@
 package com.github.andyshao.neo4j.spring.util.dao;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.neo4j.ogm.annotation.NodeEntity;
 
 import com.github.andyshao.neo4j.annotation.SqlClip;
+import com.github.andyshao.reflect.FieldOperation;
+import com.google.common.collect.Maps;
 
 public class CrudDatoClips {
     @SqlClip(sqlClipName = "saveSelective")
@@ -13,7 +20,24 @@ public class CrudDatoClips {
         else label = nodeEntity.label();
         
         StringBuilder query = new StringBuilder("CREATE (n:").append(label).append(")");
-        //TODO do something...
+        Field[] fields = FieldOperation.superGetDeclaredFields(data.getClass());
+        Map<String, Field> tmp = Maps.newHashMap();
+        for(Field field : fields) {
+            int modifiers = field.getModifiers();
+            if(!(Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers))) 
+                tmp.put(field.getName() , field);
+        }
+        if(!tmp.isEmpty()) {
+            boolean isHeader = true;
+            for(Entry<String , Field> entry : tmp.entrySet()) {
+                if(isHeader) {
+                    isHeader = false;
+                    query.append(" SET n.").append(entry.getKey()).append(" = ");
+                } else {
+                    query.append(", n.").append(entry.getKey()).append(" = ");
+                }
+            }
+        }
         query.append(" RETURN n");
         return query.toString();
     }
