9cb8b6ab5039274a7c0c0a83a29810f10ff19da5
Andy-Shao <hj13035952619@126.com>
2018-08-08 13:40:51 +0800

    fix bugs

--- neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDao.java
+++ neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDao.java
@@ -1,6 +1,7 @@
 package com.github.andyshao.neo4j.spring.util.dao;
 
 import com.github.andyshao.neo4j.annotation.Create;
+import com.github.andyshao.neo4j.annotation.Match;
 import com.github.andyshao.neo4j.annotation.Neo4jDao;
 import com.github.andyshao.neo4j.annotation.SqlInject;
 
@@ -8,6 +9,7 @@ import com.github.andyshao.neo4j.annotation.SqlInject;
 public interface CrudDao<D, K> {
     @Create(sqlInject = @SqlInject(sqlClipName = "saveSelective"))
     D saveSelective(D data);
+    @Match(sqlInject = @SqlInject(sqlClipName = "findByPk"))
     D findByPk(K pk);
     D updateSelective(D data);
     D saveOrUpdateSelective(D data);
--- neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDatoClips.java
+++ neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/util/dao/CrudDatoClips.java
@@ -2,6 +2,7 @@ package com.github.andyshao.neo4j.spring.util.dao;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -9,36 +10,53 @@ import org.neo4j.ogm.annotation.NodeEntity;
 
 import com.github.andyshao.neo4j.annotation.SqlClip;
 import com.github.andyshao.reflect.FieldOperation;
-import com.google.common.collect.Maps;
 
 public class CrudDatoClips {
     @SqlClip(sqlClipName = "saveSelective")
     public <D> String saveSelective(D data) {
-        String label;
-        NodeEntity nodeEntity = data.getClass().getAnnotation(NodeEntity.class);
-        if(nodeEntity == null) label = data.getClass().getSimpleName();
-        else label = nodeEntity.label();
+        String label = findLabel(data.getClass());
         
         StringBuilder query = new StringBuilder("CREATE (n:").append(label).append(")");
         Field[] fields = FieldOperation.superGetDeclaredFields(data.getClass());
-        Map<String, Field> tmp = Maps.newHashMap();
+        boolean isHeader = false;
         for(Field field : fields) {
             int modifiers = field.getModifiers();
-            if(!(Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers))) 
-                tmp.put(field.getName() , field);
-        }
-        if(!tmp.isEmpty()) {
-            boolean isHeader = true;
-            for(Entry<String , Field> entry : tmp.entrySet()) {
-                if(isHeader) {
-                    isHeader = false;
-                    query.append(" SET n.").append(entry.getKey()).append(" = ");
-                } else {
-                    query.append(", n.").append(entry.getKey()).append(" = ");
-                }
+            if(Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers)) continue;
+            
+            if(isHeader) {
+                isHeader = false;
+                query.append(" SET n.").append(field.getName()).append(" = ");
+                //TODO
+            } else {
+                query.append(", n.").append(field.getName()).append(" = ");
+                //TODO
             }
         }
         query.append(" RETURN n");
+        return query.toString();
+    }
+
+    public static final String findLabel(Class<? extends Object> clazz) {
+        String label;
+        NodeEntity nodeEntity = clazz.getAnnotation(NodeEntity.class);
+        if(nodeEntity == null) label = clazz.getSimpleName();
+        else label = nodeEntity.label();
+        return label;
+    }
+    
+    @SqlClip(sqlClipName = "findByPk")
+    public <K> String findByPk(K k) {
+        String label = findLabel(k.getClass());
+        StringBuilder query = new StringBuilder("MATCH (n:").append(label).append(") WHERE 1=1");
+        Field[] fields = FieldOperation.superGetDeclaredFields(k.getClass());
+        Arrays.stream(fields)
+            .filter(it -> {
+                int modifiers = it.getModifiers();
+                return !(Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers));
+            }).forEach(it -> {
+                query.append(", n.").append(it.getName()).append(" = ");
+                //TODO
+            });
         return query.toString();
     }
 }
