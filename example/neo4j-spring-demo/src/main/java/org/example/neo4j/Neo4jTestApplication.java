bb1f8da045cc90a8b66c05de47501bb2369ea424
Andy-Shao <hj13035952619@126.com>
2018-09-06 14:21:12 +0800

    fix bugs

--- example/neo4j-spring-demo/src/main/java/org/example/neo4j/Neo4jTestApplication.java
+++ example/neo4j-spring-demo/src/main/java/org/example/neo4j/Neo4jTestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableNeo4jDao(packageClasses = PersonDao.class)
 public class Neo4jTestApplication {
 	public static void main(String[] args){
 		SpringApplication.run(Neo4jTestApplication.class, args);
