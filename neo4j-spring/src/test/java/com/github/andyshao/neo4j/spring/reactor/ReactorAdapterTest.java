0f486485da9cc982dd194b755382b41cdf6c14e8
Andy-Shao <hj13035952619@16.com>
2019-12-13 11:50:42 +0000

    fix bugs

--- neo4j-spring/src/test/java/com/github/andyshao/neo4j/spring/reactor/ReactorAdapterTest.java
+++ neo4j-spring/src/test/java/com/github/andyshao/neo4j/spring/reactor/ReactorAdapterTest.java
@@ -3,13 +3,11 @@ package com.github.andyshao.neo4j.spring.reactor;
 import java.util.concurrent.CompletableFuture;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import com.github.andyshao.spring.reactor.MonoOperation;
 
 class ReactorAdapterTest {
 
-	@Test
 	void convertMono() {
 		CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> {
 			return "123";
