139c1180be14b239bc7da5ef1e4dd6014009895c
Andy Shao <andyshao@Andys-MacBook-Pro.local>
2020-09-01 20:30:27 +0800

    fix bugs

--- neo4j-core/src/main/java/com/github/andyshao/neo4j/process/ClassPathAnnotationDaoScanner.java
+++ neo4j-core/src/main/java/com/github/andyshao/neo4j/process/ClassPathAnnotationDaoScanner.java
@@ -2,11 +2,9 @@ package com.github.andyshao.neo4j.process;
 
 import com.github.andyshao.neo4j.domain.Neo4jDao;
 import com.github.andyshao.neo4j.domain.analysis.Neo4jDaoAnalysis;
-import com.google.common.collect.Maps;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -18,12 +16,7 @@ import java.util.stream.Collectors;
  * @author Andy.Shao
  */
 public class ClassPathAnnotationDaoScanner implements DaoScanner{
-    private String[] packageRegexes;
     private Package[] pkgs;
-
-    public ClassPathAnnotationDaoScanner(String... packageRegexes) {
-        this.packageRegexes = packageRegexes;
-    }
 
     public ClassPathAnnotationDaoScanner(Package... pkgs) {
         this.pkgs = pkgs;
@@ -31,15 +24,8 @@ public class ClassPathAnnotationDaoScanner implements DaoScanner{
 
     @Override
     public Map<String, Neo4jDao> scan() {
-        if(Objects.nonNull(this.packageRegexes)) {
-            return Arrays.stream(this.packageRegexes)
-                .flatMap(packageRegex -> Neo4jDaoAnalysis.analyseDaoFromPackageRegex(packageRegex).stream())
+        return Arrays.stream(this.pkgs)
+                .flatMap(pkg -> Neo4jDaoAnalysis.analyseDaoFromOnePackage(pkg).stream())
                 .collect(Collectors.toMap(Neo4jDao::getDaoId, it -> it));
-        } else if(Objects.nonNull(this.pkgs)) {
-            return Arrays.stream(this.pkgs)
-                    .flatMap(pkg -> Neo4jDaoAnalysis.analyseDaoFromOnePackage(pkg).stream())
-                    .collect(Collectors.toMap(Neo4jDao::getDaoId, it -> it));
-        }
-        return Maps.newHashMap();
     }
 }
---
+++ neo4j-spring/src/main/java/com/github/andyshao/neo4j/spring/process/ClassPathAnnotationDaoScanner.java
@@ -0,0 +1,47 @@
+package com.github.andyshao.neo4j.spring.process;
+
+import com.github.andyshao.neo4j.domain.Neo4jDao;
+import com.github.andyshao.neo4j.domain.analysis.Neo4jDaoAnalysis;
+import com.github.andyshao.neo4j.process.DaoScanner;
+import com.google.common.collect.Maps;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Copyright: Copyright(c) 2020/9/1
+ * Encoding: UNIX UTF-8
+ *
+ * @author Andy.Shao
+ */
+public class ClassPathAnnotationDaoScanner implements DaoScanner {
+    private String[] basePackages;
+    private Package[] pkgs;
+
+    public ClassPathAnnotationDaoScanner(String... basePackages) {
+        this.basePackages = basePackages;
+    }
+
+    public ClassPathAnnotationDaoScanner(Package... pkgs) {
+        this.pkgs = pkgs;
+    }
+
+    @Override
+    public Map<String, Neo4jDao> scan() {
+        if(Objects.nonNull(this.basePackages)) {
+            //TODO
+            return Arrays.stream(this.basePackages)
+                    .flatMap(packageRegex -> Neo4jDaoAnalysis.analyseDaoFromPackageRegex(packageRegex).stream())
+                    .collect(Collectors.toMap(Neo4jDao::getDaoId, it -> it));
+        } else if(Objects.nonNull(this.pkgs)) {
+            return Arrays.stream(this.pkgs)
+                    .flatMap(pkg -> Neo4jDaoAnalysis.analyseDaoFromOnePackage(pkg).stream())
+                    .collect(Collectors.toMap(Neo4jDao::getDaoId, it -> it));
+        }
+        return Maps.newHashMap();
+    }
+}
