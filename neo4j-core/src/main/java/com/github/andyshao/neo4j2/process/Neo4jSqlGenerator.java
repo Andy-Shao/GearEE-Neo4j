c488301796918de9cac6b368dde0b661752cd38a
Andy Shao <andyshao@Andys-MacBook-Pro.local>
2021-05-21 22:54:50 +0100

    adding generator

---
+++ neo4j-core/src/main/java/com/github/andyshao/neo4j2/process/Neo4jSqlGenerator.java
@@ -0,0 +1,21 @@
+package com.github.andyshao.neo4j2.process;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Copyright: Copyright(c) 2021/5/21
+ * Encoding: UNIX UTF-8
+ *
+ * @author Andy.Shao
+ */
+public class Neo4jSqlGenerator {
+    public String generateMatchSql(MatchSql sql) {
+        //TODO
+        return null;
+    }
+
+    public String generateCreateSql(CreateSql sql) {
+        //TODO
+        return null;
+    }
+}
